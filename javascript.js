9f58d7266f88a45b27c86b6a850fddab2e283554
pit <plcarmona@uc.cl>
2022-03-20 14:27:57 -0300

    js dom manipulation

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 
 function computerPlay(){
-    r=Math.floor(Math.random() * 3);
-    if(r==1){
-         r="Rock";
-    }
-    else if(r==2){
-        r="Paper";
-    }
-    else if(r==0){
-        r="Tijeras";
-    }
-    return r;
+        r=Math.floor(Math.random() * 3);
+
+        if(r==1){
+            r="Rock";
+        }
+        else if(r==2){
+            r="Paper";
+        }
+        else if(r==0){
+            r="Tijeras";
+        }
+
+        return r;
     }
     
     function playRound(player,computer){
@@ -23,21 +25,58 @@ function computerPlay(){
     else{
         text="computer wins";
     }
+
     return text;
     }
-    function oneGame(){
-    let c=0;
-    let p=0;
-    for (let i = 0; i < 5; i++) {
-        let text=prompt("Elegir","Rock, Paper or Tijeras");
-        text=playRound(text,computerPlay);
+    
+    
+
+    function oneGame(play){
+        let computer= computerPlay();
+        let text=playRound(play,computer);
         if(text=="player wins"){
             p++;
         }
         else if(text="computer wins"){
             c++;
         }
-     }
+
+        marcador.textContent= "player:"+p+" // computer"+ c;
+        if(p==5 || c==5){
+            p=0;
+            c=0;
+            let ganador="";
+            if(c>=p){ 
+                ganador=" computador";
+            } 
+            else {
+                gandor=" player";
+            }
+            marcador.textContent= "Ha ganado"+ ganador;
+            
+            
+        }
     
-     document.getElementById("demo").innerHTML = "player:"+p+" // computer:"+ c;
     }
+
+    const demo=document.querySelector('#demo');
+    const marcador=document.createElement('marcador');
+    marcador.style.color='white';
+    marcador.textContent= "player:0"+ "computer:0";
+    demo.appendChild(marcador);
+    let c=0;
+    let p=0;
+ 
+
+    const btn1 = document.querySelector('#bttn1');
+    btn1.addEventListener('click', () => {
+    oneGame("Rock");
+    });
+    const btn2 = document.querySelector('#bttn2');
+    btn2.addEventListener('click', () => {
+    oneGame("Paper");
+    });
+    const btn3 = document.querySelector('#bttn3');
+    btn3.addEventListener('click', () => {
+    oneGame("Tijeras");
+    });
